4d5b315ad6d7ea87f013d749546e51f606778d6d
KelynPNjeri <kelynpaul20.kp@gmail.com>
2018-11-29 09:00:54 +0300

    [Finishes #162220643] Writing Tests...

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,9 +1,5 @@
 """Creating Blueprints for the Version 1 of the API"""
+from flask_restplus import Api
 from flask import Blueprint
 
 # Local Imports
-from .views.comments_view import CommentList, Comment
-from .views.posts_view import PostList, Post
-from .views.auth_view import UserRegistration, UserLogin
-
-version1 = Blueprint('Version 1', __name__, url_prefix='/api/v1')
--- app/api/v1/models/posts_model.py
+++
@@ -1,3 +0,0 @@
-"""Posts Model."""
-class PostsOps:
-    pass
